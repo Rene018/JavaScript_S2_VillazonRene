06e4c0343625681ecca1611d09a826e20cf1d0fa
Rene018 <rene18agosto@gmail.com>
2025-07-29 22:27:04 -0500

    feat: 🔥 add day8

--- Dia8/main.js
+++ Dia8/main.js
@@ -1,3 +1,67 @@
+function makeCard(character) {
+  const cardContainer = document.querySelector("#all-data");
+
+  cardContainer.innerHTML = `<div class="card">
+                <div>
+                    <p>name</p>
+                    <p>${character.name}</p>
+                </div>
+
+                <div>
+                    <p>height</p>
+                    <p>${character.height}</p>
+                </div>
+
+                <div>
+                    <p>mass</p>
+                    <p>${character.mass}</p>
+                </div>
+                <div>
+                    <p>hair_color</p>
+                    <p>${character.hair_color}</p>
+                </div>
+
+                <div>
+                    <p>skin_color</p>
+                    <p>${character.skin_color}</p>
+                </div>
+
+                <div>
+                    <p>eye_color</p>
+                    <p>${character.eye_color}</p>
+                </div>
+                <div>
+                    <p>birth_year</p>
+                    <p>${character.birth_year}</p>
+                </div>
+
+                <div>
+                    <p>Mass</p>
+                    <p>${character.mass}</p>
+                </div>
+
+                <div>
+                    <p>gender</p>
+                    <p>${character.gender}</p>
+                </div>
+                <div>
+                    <p>homeworld</p>
+                    <p>${character.homeworld}</p>
+                </div>
+
+                <div>
+                    <p>films</p>
+                    <p${character.films.join(", ")}</p>
+                </div>
+
+                <div>
+                    <p>species</p>
+                    <p>${character.species}</p>
+                </div>
+
+            </div>`;
+}
+
 function getSingleCharacter() {
   let getCharacterById = new XMLHttpRequest();
   let id = prompt("Introduce el id del personaje que quieres buscar:");
@@ -8,7 +72,8 @@ function getSingleCharacter() {
       if (this.readyState == 4 && this.status == 200) {
         let response = JSON.parse(getCharacterById.responseText);
         let character = response;
-        console.log(character);
+        console.log(character.films.join(", "));
+        makeCard(character);
       }
     } catch (error) {
       console.log(error);
@@ -34,5 +99,4 @@ function main() {
   getAllCharacters.open("GET", "https://swapi.py4e.com/api/people/", true);
   getAllCharacters.send();
 }
-
 getSingleCharacter();
